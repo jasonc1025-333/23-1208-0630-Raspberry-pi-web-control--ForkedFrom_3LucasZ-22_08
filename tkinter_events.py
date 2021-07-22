4cbbf8ed49a88930575e1457ff9dae1dab804b9e
3LucasZ <lucas.j.zheng@gmail.com>
2021-07-22 14:25:16 -0700

    Update
    new files

---
+++ tkinter_events.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+import tkinter as tk
+
+def say_hello():
+    print("hello")
+
+r = tk.Tk()
+r.title('Hello World')
+
+fBtn = tk.Button(r, text='Forward', width = 15, height = 10, command=say_hello)
+fBtn.pack()
+lBtn = tk.Button(r, text='Left', width = 15, height = 10, command=say_hello)
+lBtn.pack()
+
+r.mainloop()
