27f2aa9ed7ba31de33a147e12ae97eeb8b2a4f02
3LucasZ <lucas.j.zheng@gmail.com>
2021-07-22 18:41:35 -0700

    Update

--- flask_motor_control/main.py
+++ flask_motor_control/main.py
@@ -7,28 +7,28 @@ import RPi.GPIO as GPIO
 import time
 
 app = Flask(__name__)
-motorsEnabled = False
+
 PIN_I2C6_POWER_ENABLE = 17
 bus = smbus.SMBus(3)
 DEVICE_ADDRESS = 0x53
 GPIO.setmode(GPIO.BCM)
+time.sleep(0.1) #important
 GPIO.setup(PIN_I2C6_POWER_ENABLE, GPIO.OUT)
 time.sleep(0.1) #important
 
 
 @app.route("/", methods=["GET","POST"])
 def home():
-    global motorsEnabled
+    
     if request.method == "POST":
-        motorsEnabled = not motorsEnabled
-    if motorsEnabled:
-        GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.HIGH)
-    else:
-        GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.LOW)
+        if request.form['motorOn']:
+            GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.HIGH)
+        else if request.form['motorOff']:
+            GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.LOW)
   
     return render_template('index.html', motorsEnabled=motorsEnabled)
 
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=80, debug=True)
-   
+    #will run on: http://192.168.1.162:8080/
+    app.run(host='0.0.0.0', port=8080, debug=True)
