71d14fe1b7c628f4bd979e83e93e0575b87494ec
3LucasZ <lucas.j.zheng@gmail.com>
2021-07-23 16:58:06 -0700

    new experiments

--- get_camera_data/camera_picture.py
+++ get_camera_data/camera_picture.py
@@ -1,9 +1,21 @@
-from picamera import PiCamera
+#from picamera import PiCamera
 from time import sleep
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def home():
+    newPicture()
+    return "<img src='static/picture.jpg'>"
 
 camera = PiCamera()
+def newPicture():
+    camera.start_preview()
+    sleep(2)
+    camera.capture('/static/picture.jpg')
+    camera.stop_preview()
 
-camera.start_preview()
-sleep(5)
-camera.capture('/home/pi/Desktop/image.jpg')
-camera.stop_preview()
+if __name__ == "__main__":
+    #will run on: http://192.168.1.162:8080/
+    app.run(host='0.0.0.0', port=8080, debug=True)
