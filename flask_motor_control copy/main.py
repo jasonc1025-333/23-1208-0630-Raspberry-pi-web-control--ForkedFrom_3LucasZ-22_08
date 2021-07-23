4fa4eed57205bd1fdda9ef662264470743c1325d
3LucasZ <lucas.j.zheng@gmail.com>
2021-07-22 19:02:03 -0700

    update

--- flask_motor_control copy/main.py
+++ flask_motor_control copy/main.py
@@ -1,21 +1,8 @@
 #! /usr/bin/env python
 from flask import Flask, render_template, request
-import int_to_byte
-import smbus
-import math
-import RPi.GPIO as GPIO
-import time
 
 app = Flask(__name__)
 motorsEnabled = False
-
-PIN_I2C6_POWER_ENABLE = 17
-bus = smbus.SMBus(3)
-DEVICE_ADDRESS = 0x53
-GPIO.setmode(GPIO.BCM)
-time.sleep(0.1) #important
-GPIO.setup(PIN_I2C6_POWER_ENABLE, GPIO.OUT)
-time.sleep(0.1) #important
 
 
 @app.route("/", methods=["GET","POST"])
@@ -25,10 +12,8 @@ def home():
         action = request.form['action']
         if action == 'motorOn':
             motorsEnabled = True
-            GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.HIGH)
         elif action == 'motorOff':
             motorsEnabled = False
-            GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.LOW)
   
     return render_template('index.html', motorsEnabled=motorsEnabled)
 
