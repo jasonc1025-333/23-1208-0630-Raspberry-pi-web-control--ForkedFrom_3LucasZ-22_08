9248b7595d3cfef8fda817123b8a25739a0c7a0b
3LucasZ <lucas.j.zheng@gmail.com>
2021-07-23 17:06:16 -0700

    Update camera_picture.py

--- get_camera_data/camera_picture.py
+++ get_camera_data/camera_picture.py
@@ -3,18 +3,20 @@ from time import sleep
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+camera = PiCamera()
+camera.resolution = (300,300)
+camera.start_preview()
 
 @app.route("/", methods=["GET","POST"])
 def home():
     newPicture()
     return "<img src='static/picture.jpg'>"
 
-camera = PiCamera()
+
 def newPicture():
-    camera.start_preview()
     sleep(2)
-    camera.capture('/static/picture.jpg')
-    camera.stop_preview()
+    camera.capture('static/picture.jpg')
+    
 
 if __name__ == "__main__":
     #will run on: http://192.168.1.162:8080/
