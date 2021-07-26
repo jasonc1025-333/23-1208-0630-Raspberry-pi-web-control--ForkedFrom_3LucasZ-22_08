d370b6e8fffdd842567370fb2ea86241b1866574
3LucasZ <lucas.j.zheng@gmail.com>
2021-07-26 15:15:56 -0700

    tiny update

--- flask_keyboard_motor_control/int_to_byte.py
+++ flask_keyboard_motor_control/int_to_byte.py
@@ -1,20 +1,19 @@
-#! /usr/bin/env python
 from textwrap import wrap
 def int_to_byte_array(num):
    if num < 0:
       hexStr = hex((1<<32) + num)
    else:
       hexStr = hex(num)
-   hexStr = hexStr.rstrip("L")      #sometimes the hex conversion adds a 'L' to the end. Remove it
+   hexStr = hexStr.rstrip("L")
 
-   padded = str.format('{:08X}',int(hexStr,16))   #pad out to 8 hex characters
-   padded = wrap(padded,2)                     #split into pairs of hex chars ie bytes
+   padded = str.format('{:08X}',int(hexStr,16))
+   padded = wrap(padded,2)
 
    array = [0,0,0,0]
    for x in range(4):
       array[x] = int(padded[x],16)
 
-   array.reverse()      #reverse the list. We will send LSB first
+   array.reverse()
 
    return array
 
