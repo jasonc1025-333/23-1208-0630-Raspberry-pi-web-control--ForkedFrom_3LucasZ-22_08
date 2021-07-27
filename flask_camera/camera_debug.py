240a9e0f6ca1ad3e3c6fc333115568ee8fd97dcc
3LucasZ <lucas.j.zheng@gmail.com>
2021-07-26 19:57:07 -0700

    Update camera_debug.py

--- flask_camera/camera_debug.py
+++ flask_camera/camera_debug.py
@@ -6,7 +6,7 @@ from picamera import PiCamera
 camera = PiCamera()
 camera.resolution = (300, 300)
 camera.start_preview()
-sleep(2)
+time.sleep(2)
 camera.capture('foo.jpg')
 
 app = Flask(__name__)
