b452ef6c93c7fa81a0508d29a7c1d7dfd3a47673
3LucasZ <lucas.j.zheng@gmail.com>
2021-07-26 20:02:41 -0700

    Update camera_debug.py

--- flask_camera/camera_debug.py
+++ flask_camera/camera_debug.py
@@ -6,10 +6,6 @@ from picamera import PiCamera
 app = Flask(__name__)
 
 
-camera = PiCamera()
-camera.resolution = (300, 300)
-
-
 @app.route("/")
 def home():
     return "HELLO WORLD"
@@ -18,3 +14,7 @@ def home():
 if __name__ == "__main__":
     #will run on: http://192.168.1.162:8080/
     app.run(host='0.0.0.0', port=8080, debug=True)
+
+    try:
+        camera = PiCamera()
+        camera.resolution = (300, 300)
