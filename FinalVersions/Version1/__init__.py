79e91525467584c7bf50a645240638ef40b21a9d
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-01 16:54:56 -0700

    Update __init__.py

--- FinalVersions/Version1/__init__.py
+++ FinalVersions/Version1/__init__.py
@@ -34,7 +34,7 @@ camera = cv2.VideoCapture(0)
 time.sleep(1)
 camera.set(3, 64)
 camera.set(4, 64)
-FPS = 15
+FPS = 5
 sentCamera = False
 
 
@@ -79,7 +79,7 @@ def send_lidar():
     try:
         while True:
             #get the most recent scans from scan generator
-            for scan in lidar.iter_scans(1): 
+            for scan in lidar.iter_scans(3): 
                 #scan has array of points
                 #each point has 3 properties: quality, angle, distance
                 for (_, angle, distance) in scan:
@@ -88,8 +88,7 @@ def send_lidar():
                 #send all clients scan_data array
                 #print(scan_data)
                 emit("scanData", scan_data)
-                socketio.sleep(0)
-            socketio.sleep(0.5)
+                socketio.sleep(0.5)
     except KeyboardInterrupt:
         print('Stopping.')
 
