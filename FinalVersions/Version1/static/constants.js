2b745021b35f40d9257a2063289ecf4781d5d010
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-01 16:31:34 -0700

    Version 1 Update

---
+++ FinalVersions/Version1/static/constants.js
@@ -0,0 +1,13 @@
+//LIDAR GRAPHING CONSTANTS
+//measurement: cm
+const MIN_DISTANCE = 0;
+const MAX_DISTANCE = 6000;
+
+const ROBOT_WIDTH = 230;
+const ROBOT_LENGTH = 300;
+
+//measurement: pixel
+const SCREEN_WIDTH = 700;
+
+//custom measurement
+const PIXEL_PER_CM = SCREEN_WIDTH / MAX_DISTANCE;
--- FinalVersions/Version1/static/websocket_handler.js
+++ FinalVersions/Version1/static/websocket_handler.js
@@ -1,49 +1,121 @@
 //SETUP
-//websocket
+//enable websocket
 var socket = io();
-//livestream
+
+//lidar graph widget setup
+var lidarGraph = document.getElementById('lidarGraph');
+var ctx = lidarGraph.getContext("2d");
+var pixelSize = 1;
+
+//livestream widget setup
 var livestream = document.getElementById('livestream')
-//speed
+
+//speed widget setup
 var speedSlider = document.getElementById("speedSlider");
 var speedSpan = document.getElementById("speedSpan");
-speedSpan.innerHTML = speedSlider.value
+speedSpan.innerHTML = speedSlider.value;
+
+//pixel size input setup
+var pixelSizeInput = document.getElementById("pixelSizeInput");
+
 
 //LISTENERS
 socket.on('connect', function(){
     console.log('connected');
-    socket.emit('needCamera')
+    socket.emit('needCamera');
+    socket.emit('needLidar');
 });
+
 socket.on('disconnect', function(){
     console.log('disconnected');
 });
+
 socket.on('jpg_string', function(data){
     livestream.src = 'data:image/jpeg;base64,' + data;
 })
 
+socket.on('scanData', function(data) {
+    //clear the old canvas
+    ctx.clearRect(0, 0, graph.width, graph.height);
+
+    //draw the robot shape
+    ctx.strokeStyle = "#0000FF";
+    ctx.strokeRect(
+        (SCREEN_WIDTH / 2) - (ROBOT_WIDTH * PIXEL_PER_CM / 2), 
+        (SCREEN_WIDTH / 2) - (ROBOT_LENGTH * PIXEL_PER_CM / 2), 
+        ROBOT_WIDTH * PIXEL_PER_CM, 
+        ROBOT_LENGTH * PIXEL_PER_CM);
+
+    //Plot every array point
+    for (let i=0;i<360;i++) {
+        //lidar is turned 90 degrees too much to the right
+        radians = (i - 90) * (Math.PI / 180);
+        distance = data[i];
+        x = distance * Math.cos(radians);
+        y = distance * Math.sin(radians);
+        scaled_x = (SCREEN_WIDTH / 2) + (x * PIXEL_PER_CM);
+        scaled_y = (SCREEN_WIDTH / 2) + (y * PIXEL_PER_CM);
+
+        //This tells us where angle 0 is
+        if (i == 0) {
+            //graph special point
+            ctx.fillStyle = "#FF0000";
+            ctx.fillRect(
+                scaled_x - pixelSize, 
+                scaled_y - pixelSize, 
+                pixelSize * 2, 
+                pixelSize * 2);
+        }
+        else {
+            //graph point
+            ctx.fillStyle = "#000000";
+            ctx.fillRect(
+                scaled_x - (pixelSize / 2), 
+                scaled_y - (pixelSize / 2), 
+                pixelSize, 
+                pixelSize);
+        }
+    }
+});
+
+
 //SENDERS
+//button presses
 function motorsOn(){
     socket.emit('motorsOn');
 }
+
 function motorsOff(){
     socket.emit('motorsOff');
 }
+
 function turnLeft(){
     socket.emit('turnLeft');
 }
+
 function turnRight(){
     socket.emit('turnRight');
 }
+
 function forward(){
     socket.emit('forward');
 }
+
 function backward(){
     socket.emit('backward');
 }
+
 function stopMotors(){
     socket.emit('stopMotors')
 }
-//SPEED BAR
+
+//speed slider
 speedSlider.oninput = function() {
     speedSpan.innerHTML = this.value;
     socket.emit('setSpeed', this.value)
+}
+
+//pixel size input
+pixelSizeInput.oninput = function() {
+    pixelSize = parseInt(this.value);
 }
