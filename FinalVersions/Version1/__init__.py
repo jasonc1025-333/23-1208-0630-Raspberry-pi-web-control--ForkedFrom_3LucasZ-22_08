0bdfef81a02c0cae81333a9dd0343cef82177ebf
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-01 17:51:38 -0700

    Update __init__.py

--- FinalVersions/Version1/__init__.py
+++ FinalVersions/Version1/__init__.py
@@ -88,7 +88,7 @@ def send_lidar():
                 #send all clients scan_data array
                 #print(scan_data)
                 emit("scanData", scan_data)
-                socketio.sleep(0)
+                socketio.sleep(0.5)
     except KeyboardInterrupt:
         print('Stopping.')
 
