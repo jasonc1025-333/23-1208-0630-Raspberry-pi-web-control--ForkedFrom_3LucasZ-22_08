9f16b787f49f187516b9cc8c0020628cfdda6fcf
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-07 01:44:52 -0700

    Update motor_stop_basic.py

--- No-UI-Robot/regular_motor_control/motor_stop_basic.py
+++ No-UI-Robot/regular_motor_control/motor_stop_basic.py
@@ -1,14 +1,7 @@
-#! /usr/bin/env python
-import smbus2 as smbus
-import math
 import RPi.GPIO as GPIO
 import time
-from textwrap import wrap
 
 PIN_I2C6_POWER_ENABLE = 17
-
-bus = smbus.SMBus(1)      #this is I2C6 on the pi4 for some reason
-DEVICE_ADDRESS = 0x53
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN_I2C6_POWER_ENABLE, GPIO.OUT)
