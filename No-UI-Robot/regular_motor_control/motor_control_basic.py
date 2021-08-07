24fcdc405b4a6fdf09e8bc881630ba66cddb7318
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-07 01:04:27 -0700

    Update motor_control_basic.py

--- No-UI-Robot/regular_motor_control/motor_control_basic.py
+++ No-UI-Robot/regular_motor_control/motor_control_basic.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-import smbus
+import smbus2 as smbus
 import math
 import RPi.GPIO as GPIO
 import time
@@ -16,8 +16,8 @@ GPIO.setup(PIN_I2C6_POWER_ENABLE, GPIO.OUT)
 time.sleep(0.1) #EXTREMELY important
 GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.HIGH)
 time.sleep(0.1) #EXTREMELY important
-bus.write_i2c_block_data(DEVICE_ADDRESS,3,int_to_byte.int_to_byte_array(50))
-bus.write_i2c_block_data(DEVICE_ADDRESS,4,int_to_byte.int_to_byte_array(50))
+bus.write_i2c_block_data(DEVICE_ADDRESS,3,int_to_byte.int_to_byte_array(10))
+bus.write_i2c_block_data(DEVICE_ADDRESS,4,int_to_byte.int_to_byte_array(10))
 time.sleep(1)
 GPIO.cleanup()
 
