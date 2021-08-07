9cc1ec118bb4aa79dc16bbee4672c80ac4f22bca
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-07 14:38:47 -0700

    Update __init__.py

--- FinalVersions/Version1/__init__.py
+++ FinalVersions/Version1/__init__.py
@@ -44,7 +44,7 @@ sentCamera = False
 
 #motor setup
 PIN_I2C6_POWER_ENABLE = 17
-bus = smbus.SMBus(3)
+bus = smbus.SMBus(1)
 DEVICE_ADDRESS = 0x53
 GPIO.setmode(GPIO.BCM)
 time.sleep(0.1) #important
