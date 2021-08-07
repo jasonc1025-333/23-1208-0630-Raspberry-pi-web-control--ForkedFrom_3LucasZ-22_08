ff5237394218540f5f4342201713b369b1333e19
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-07 15:54:22 -0700

    Update motor_control_basic.py

--- No-UI-Robot/regular_motor_control/motor_control_basic.py
+++ No-UI-Robot/regular_motor_control/motor_control_basic.py
@@ -6,7 +6,7 @@ import time
 from textwrap import wrap
 import int_to_byte
 
-PIN_I2C6_POWER_ENABLE = 17
+PIN_I2C6_POWER_ENABLE = 11
 
 bus = smbus.SMBus(1)      #this is I2C6 on the pi4 for some reason
 DEVICE_ADDRESS = 0x53
@@ -16,8 +16,8 @@ GPIO.setup(PIN_I2C6_POWER_ENABLE, GPIO.OUT)
 time.sleep(0.1) #EXTREMELY important
 GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.HIGH)
 time.sleep(0.1) #EXTREMELY important
-bus.write_i2c_block_data(DEVICE_ADDRESS,3,int_to_byte.int_to_byte_array(10))
-bus.write_i2c_block_data(DEVICE_ADDRESS,4,int_to_byte.int_to_byte_array(10))
+bus.write_i2c_block_data(DEVICE_ADDRESS,3,int_to_byte.int_to_byte_array(30))
+bus.write_i2c_block_data(DEVICE_ADDRESS,4,int_to_byte.int_to_byte_array(30))
 time.sleep(1)
 bus.write_i2c_block_data(DEVICE_ADDRESS,3,int_to_byte.int_to_byte_array(0))
 bus.write_i2c_block_data(DEVICE_ADDRESS,4,int_to_byte.int_to_byte_array(0))
