11860d421cd547160dbb509fb2b8742ce3eff885
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-07 20:21:59 -0700

    update

---
+++ No-UI-Robot/regular_motor_control/get_i2c_register_values.py
@@ -0,0 +1,17 @@
+import smbus2 as smbus
+import time
+import RPi.GPIO as GPIO
+
+PIN_I2C6_POWER_ENABLE = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_I2C6_POWER_ENABLE, GPIO.OUT)
+time.sleep(0.1)
+GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.HIGH)
+time.sleep(0.1)
+
+bus = smbus.SMBus(6)
+DEVICE_ADDRESS = 0x53
+
+for i in range(10):
+    print(bus.read_i2c_block_data(DEVICE_ADDRESS,i,8))
--- No-UI-Robot/regular_motor_control/motor_control_basic.py
+++ No-UI-Robot/regular_motor_control/motor_control_basic.py
@@ -1,29 +1,22 @@
 import smbus2 as smbus
-import math
 import RPi.GPIO as GPIO
 import time
-from textwrap import wrap
 import int_to_byte
 
 PIN_I2C6_POWER_ENABLE = 17
 
-bus = smbus.SMBus(6)      #this is I2C6 on the pi4 for some reason
+bus = smbus.SMBus(6)
 DEVICE_ADDRESS = 0x53
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN_I2C6_POWER_ENABLE, GPIO.OUT)
-time.sleep(0.1) #EXTREMELY important
+time.sleep(0.1)
 GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.HIGH)
-time.sleep(0.1) #EXTREMELY important
-bus.write_i2c_block_data(DEVICE_ADDRESS,1,int_to_byte.int_to_byte_array(7))
-bus.write_i2c_block_data(DEVICE_ADDRESS,2,int_to_byte.int_to_byte_array(5))
-
+time.sleep(0.1)
+#bus.write_i2c_block_data(DEVICE_ADDRESS,1,int_to_byte.int_to_byte_array(7))
+#bus.write_i2c_block_data(DEVICE_ADDRESS,2,int_to_byte.int_to_byte_array(5))
 bus.write_i2c_block_data(DEVICE_ADDRESS,3,int_to_byte.int_to_byte_array(30))
 bus.write_i2c_block_data(DEVICE_ADDRESS,4,int_to_byte.int_to_byte_array(30))
 time.sleep(3)
 bus.write_i2c_block_data(DEVICE_ADDRESS,3,int_to_byte.int_to_byte_array(0))
 bus.write_i2c_block_data(DEVICE_ADDRESS,4,int_to_byte.int_to_byte_array(0))
-time.sleep(1)
-#GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.LOW)
-#time.sleep(1)
-#GPIO.cleanup()
