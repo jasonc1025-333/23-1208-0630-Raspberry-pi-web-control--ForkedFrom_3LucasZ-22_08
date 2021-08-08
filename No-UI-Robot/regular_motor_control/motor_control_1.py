0588d735d9b2ff65e868a6f7b8d43ab7001148da
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-07 18:46:26 -0700

    Update motor_control_1.py

--- No-UI-Robot/regular_motor_control/motor_control_1.py
+++ No-UI-Robot/regular_motor_control/motor_control_1.py
@@ -3,6 +3,6 @@ import time
 bus = smbus.SMBus(1)
 DEVICE_ADDRESS = 0x53
 for i in range(0, 10):
-    bus.write_i2c_block_data(DEVICE_ADDRESS,4,[1,1,0,0,0,0,0,0])
+    bus.write_i2c_block_data(DEVICE_ADDRESS,i,[1,1,0,0,0,0,0,0])
     time.sleep(1)
-    bus.write_i2c_block_data(DEVICE_ADDRESS,4,[0,0,0,0,0,0,0,0])
+    bus.write_i2c_block_data(DEVICE_ADDRESS,i,[0,0,0,0,0,0,0,0])
