c2bc2fdc82d060542078f5180b3566fd44d82eb2
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-07 20:02:10 -0700

    Update motor_control_1.py

--- No-UI-Robot/regular_motor_control/motor_control_1.py
+++ No-UI-Robot/regular_motor_control/motor_control_1.py
@@ -9,4 +9,4 @@ time.sleep(1)
 bus.write_i2c_block_data(DEVICE_ADDRESS,3,[0,0,0,0,0,0,0,0])
 bus.write_i2c_block_data(DEVICE_ADDRESS,4,[0,0,0,0,0,0,0,0])
 for i in range(10):
-    print(bus.read_i2c_block_data(DEVICE_ADDRESS,i))
+    print(bus.read_i2c_block_data(DEVICE_ADDRESS,i,8))
